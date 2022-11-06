413c0dc9818da386fcd6453e169280c963cd8418
Kirubakaran Kathirvel <kirubakarankathirvel@Kirubakarans-MacBook-Air.local>
2022-11-06 16:39:18 +0530

    Day2

--- TestJavaClass/src/week1/day2/MainCalculator.java
+++ TestJavaClass/src/week1/day2/MainCalculator.java
@@ -1,14 +1,27 @@
 package week1.day2;
 
+import java.util.Scanner;
+
 public class MainCalculator {
 	
 	public static void main(String[] args)
 	{
 		TestCalculator objCalculator = new TestCalculator();
-		int addition = objCalculator.sum(12, 33, 566);
-		int subtractResult =objCalculator.sub(20, 10);
-		double multipleResult= objCalculator.multiple(90, 688);
-		float divideresult = objCalculator.divide(78, 12);
+		Scanner in = new Scanner(System.in);  
+		System.out.print("num1:"); 
+		int num1 = in.nextInt();
+		System.out.print("num2:"); 
+		int num2 = in.nextInt();
+		System.out.print("num3:");
+		int num3 = in.nextInt();
+		System.out.print("float1: ");
+		float float1 = in.nextFloat();
+		System.out.print("float2: ");
+		float float2 = in.nextFloat();
+		int addition = objCalculator.sum(num1, num2, num3);
+		int subtractResult =objCalculator.sub(num1, num2);
+		double multipleResult= objCalculator.multiple(90.24, 688.23);
+		float divideresult = objCalculator.divide(float1, float2);
 		
 		System.out.println("AdditionResult: "+addition);
 		System.out.println("Sub: "+subtractResult);
